db170ddab9d855e029f69441ae7c51299328c2d7
DiogoZMartini <diogo.81226211@gmail.com>
2023-10-05 17:36:54 -0300

    Atualização nos comandos de rolar dado e ajuda

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import random
-
 #Declarando as intenções do bot
 intents = discord.Intents.default()
 intents. message_content = True
@@ -11,7 +10,7 @@ client = discord.Client(intents=intents)
 #Quando o bot iniciar mostrara uma mensagem no terminal
 @client.event
 async def on_ready():
-    print(f"{client.user} está online !")
+    print(f'{client.user} está online !')
 
 @client.event
 async def on_message(message):
@@ -25,13 +24,27 @@ async def on_message(message):
     
 #Enviando uma mensagem privada com a lista de comandos
     if l_conteudo.startswith('!help'):
-        await message.author.send(f':notebook_with_decorative_cover: ***Lista de Comandos*** :notebook_with_decorative_cover:\n'+
-                                  '***!roll d20*** --> :game_die: Rola um dado de 20 lados :game_die:')
+        try:
+            await message.author.send(f':notebook_with_decorative_cover: ***Lista de Comandos*** :notebook_with_decorative_cover:\n'+
+                                  '***!roll d20*** --> :game_die: Rola um dado de 20 lados :game_die:\n'+
+                                  '**!ap1** Mostra uma Imagme do Primeiro Apostolo\n'+
+                                  '**!ap2** Mostra uma Imagme do Segundo Apostolo\n'+
+                                  '**!ap3** Mostra uma Imagme do Trceiro Apostolo\n'+
+                                  '**!ap4** Mostra uma Imagme do Quarto Apostolo\n'+
+                                  '**!ap5** Mostra uma Imagme do Quinto Apostolo\n'+
+                                  '**!apc5** Mostra uma Imagme do companheiro do Quinto Apostolo')
+        except discord.errors.Forbidden:
+            await message.channel.send('Não foi possivel enviar a mensagem, habilite receber mensagens de qualquer pessoa do servidor (Opções > Privacidade)')
     
 #Rolando um dado randomico no canal onde o comando foi usado
     if l_conteudo.startswith('!roll d20'):
-        await message.channel.send(f':game_die: Dados Rolados! \n'+
-                                   f'[***d20*** : {num_random}]')
+        embed_dado = discord.Embed(
+            title=':game_die: Dados Rolados!',
+            description= (f'[***d20*** : {num_random}]'),
+            color= 0x030bfc
+        )
+        embed_dado.set_footer(text=f"Dado do **{message.author}**")
+        await message.channel.send(embed=embed_dado)
 #enviando imagens
     if l_conteudo.startswith('!ap1'):
         await message.channel.send(file=discord.File('.\img\mostros/apostolo_1.png'))
